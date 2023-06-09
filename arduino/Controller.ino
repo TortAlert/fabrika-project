0eb187978824f7fd0b256609d0b5a9df15d6dd7f
Ivan Zubritskiy <60708873+TortAlert@users.noreply.github.com>
2023-06-09 11:01:29 +0300

    Add files via upload

---
+++ arduino/Controller.ino
@@ -0,0 +1,89 @@
+#include <Servo.h>
+
+
+const int EMGpin = A0;
+const int bPin_grip = 8;
+const int bPin_fuck = 9;
+const int bPin_rotate = 10;
+
+int EMGstate = 0;
+bool flag = 0;
+int bState_grip = 0;
+int bState_fuck = 0;
+int bState_rotate = 0;
+
+
+Servo rotServo1;
+Servo rotServo2;
+Servo rotServo3;
+Servo rotServo4;
+Servo rotServo5;
+Servo rotServoR;
+
+void setup() {
+  // put your setup code here, to run once:
+  delay( 3000 );
+  Serial.begin(9600);
+  
+  rotServo1.attach(2);
+  rotServo2.attach(3);
+  rotServo3.attach(4);
+  rotServo4.attach(5);
+  rotServo5.attach(6);
+  rotServoR.attach(7);
+
+  pinMode(bPin_grip, INPUT_PULLUP);
+  pinMode(bPin_fuck, INPUT_PULLUP);
+  pinMode(bPin_rotate, INPUT_PULLUP);
+
+  pinMode(EMGpin, INPUT);
+}
+
+void loop() {
+  EMGstate = analogRead(EMGpin);
+
+  if (EMGstate > 100)
+    flag = ~flag;
+
+  bState_grip = digitalRead(bPin_grip);
+  bState_fuck = digitalRead(bPin_fuck);
+  bState_rotate = digitalRead(bPin_rotate);
+
+  if (flag) {
+    rotServo1.write(180);
+    rotServo2.write(180);
+    rotServo3.write(180);
+    rotServo4.write(180);
+    rotServo5.write(180);
+  }
+  else {
+    rotServo1.write(0);
+    rotServo2.write(0);
+    rotServo3.write(0);
+    rotServo4.write(0);
+    rotServo5.write(0);
+  }
+
+  if (bState_fuck) {
+    rotServo1.write(180);
+    rotServo2.write(180);
+    rotServo3.write(0);
+    rotServo4.write(180);
+    rotServo5.write(180);
+  }
+  else {
+    rotServo1.write(0);
+    rotServo2.write(0);
+    rotServo3.write(0);
+    rotServo4.write(0);
+    rotServo5.write(0);
+  }
+
+  if (bState_rotate)
+    rotServoRwrite(180);
+  else
+    rotServoRwrite(0);
+
+  Serial.println(EMGstate);
+  delay(200);
+}
